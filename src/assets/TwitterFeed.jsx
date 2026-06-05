4867b03b68dfffd1b3bb3b6915e0abb074555fff
Ace <edisonford2006@gmail.com>
2026-06-05 14:25:22 +0100

    refactor: remove react-twitter-embed dependency and replace with iframe in TwitterFeed component

--- src/assets/TwitterFeed.jsx
+++ src/assets/TwitterFeed.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
-
 export default function TwitterFeed() {
   return (
     <div className="twitter-card">
@@ -8,19 +5,12 @@ export default function TwitterFeed() {
         <h2>LATEST <span>UPDATES</span></h2>
         <p>Stay up to date with match results and team announcements.</p>
       </div>
-
-      {/* Added the crop-to-latest class here */}
       <div className="twitter-frame crop-to-latest">
-        <TwitterTimelineEmbed
-          sourceType="profile"
-          screenName="VALORANT"
-          // Restricting the height limits how far down the timeline can generate data frames
-          options={{ height: 450 }} 
-          theme="dark"
-          noBorders={true}
-          noHeader={true}
-          noFooter={true} // Hides X's bottom "Load More" action panel
-          transparent={true}
+        <iframe
+          src="https://syndication.twitter.com/srv/timeline-profile/screen-name/AquanticusUK?dnt=true&theme=dark&chrome=noheader%20nofooter%20noborders%20transparent"
+          style={{ width: '100%', height: '450px', border: 'none', overflow: 'hidden' }}
+          scrolling="no"
+          title="Latest Updates"
         />
       </div>
     </div>
